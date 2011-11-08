04e4a84ac700083abcbc49abcf555332309d701b
paulo.ricardobm@gmail.com <paulo.ricardobm@gmail.com@eb6ea66a-d05f-b00a-ad9e-32df395f4553>
2011-11-08 11:50:56 +0000

    inicial upload

---
+++ trunk/ readexcel-mctreport/ReadExcelFile/Model.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ReadExcelFile
+{
+    class Model
+    {
+        private String modelCode;
+        private List<CA> modelCAs;
+        private CA firstCA;
+        private Int32 numberModelCAs;
+
+        public Model()
+        {
+            this.numberModelCAs = 0;
+            modelCAs = new List<CA>();
+        }
+
+        public String extractModelName(String txt)
+        {
+            String tmp = "NO MODEL NAME";
+
+            if (txt != null)
+                tmp = txt;
+            
+            // remove undesirable characters in the code portions below
+            // remove - character
+            if (tmp.Contains("-"))
+            {
+                String[] t = tmp.Split('-');
+                tmp = t[1];
+            }
+
+            // remove LG starting characters
+            if (tmp.Contains("LG"))
+                tmp = tmp.Substring(txt.IndexOf("L") + 2, tmp.Length - 2);
+
+            // remove _ character
+            if (tmp.Contains("_"))
+                tmp = tmp.Substring(0, tmp.IndexOf("_"));
+
+            // remove characters at the end of the model name
+            if (tmp.Length > 4 && !Char.IsNumber(tmp, tmp.Length - 1))
+            {
+                char[] t = tmp.ToCharArray();
+                int c = tmp.Length - 1;
+
+                for (; c >= 0; c--)
+                    if (Char.IsNumber(t[c]))
+                        break;
+
+                tmp = tmp.Substring(0, ++c);
+            }
+
+            if (tmp.Length < 1)
+                return "NO MODEL NAME";
+            else
+                return tmp;
+        }
+
+        public String ModelCode
+        {
+            get { return this.modelCode; }
+            set { this.modelCode = extractModelName(value.ToString());}
+        }
+
+        public List<CA> ModelCas
+        {
+            get { return this.modelCAs; }
+        }
+
+        public CA ModelCAs
+        {
+            get
+            {
+                firstCA = new CA();
+                foreach (CA ca in modelCAs)
+                {
+                    firstCA.ListCountryCodes = ca.ListCountryCodes;
+                    firstCA.CarrierName = ca.CarrierName;
+                    firstCA.Country = ca.Country;
+                    firstCA.MediumManMonth = ca.MediumManMonth;
+                    firstCA.ProjectStatus = ca.ProjectStatus;
+                    firstCA.PeopleReportedHours = ca.PeopleReportedHours;
+                }
+                return firstCA;
+            }
+            set
+            {
+                if (value.GetType() == typeof(CA))
+                {
+                    CA ca = new CA(value.ListCountryCodes);
+
+                    ca.CarrierName = value.CarrierName;
+                    ca.Country = value.Country;
+                    ca.MediumManMonth = value.MediumManMonth;
+                    ca.ProjectStatus = value.ProjectStatus;
+                    ca.PeopleReportedHours = value.PeopleReportedHours;
+
+                    modelCAs.Add(ca);
+                }
+
+            }
+        }
+
+    }
+}
---
+++ trunk/ readexcel-mctreport/ReadExcelFile/frmPreviewExcel.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Data.OleDb;
+
+namespace ReadExcelFile
+{
+    public partial class frmPreviewExcel : Form
+    {
+        String filePath;
+        String spreadSheet;
+
+        public frmPreviewExcel()
+        {
+            InitializeComponent();
+            filePath = null;
+        }
+
+        public frmPreviewExcel(String filePath, String spreadSheet)
+        {
+            InitializeComponent();
+            this.filePath = filePath;
+            this.spreadSheet = spreadSheet;
+        }
+
+        private void frmPreviewExcel_Load(object sender, EventArgs e)
+        {
+            try
+            {
+                String strConnection = "Provider=Microsoft.Jet.OLEDB.4.0;" +
+                                       "Data Source=" + filePath + "; Jet OLEDB:Engine Type=5;Extended Properties=Excel 8.0;";
+
+                OleDbConnection oDbConn = new OleDbConnection(strConnection);
+                oDbConn.Open();
+                OleDbCommand cmdSelect = new OleDbCommand(@"SELECT * FROM [" + spreadSheet + "$]", oDbConn);
+                OleDbDataAdapter oDbDataAdapter = new OleDbDataAdapter();
+                oDbDataAdapter.SelectCommand = cmdSelect;
+                DataTable dTable = new DataTable();
+                oDbDataAdapter.Fill(dTable);
+                oDbConn.Close();
+                oDbDataAdapter = null;
+
+                dataGridSpreadSheetOverview.DataSource = dTable;
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void pbOk_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
